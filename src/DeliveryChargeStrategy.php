939d9e692c0d69324e18764920517a64d624fb7f
lennybeadle <Lenny@superreel.ai>
2025-06-27 15:35:29 +0100

    add delivery charge strategy

---
+++ src/DeliveryChargeStrategy.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme;
+
+final class DeliveryChargeStrategy implements DeliveryChargeStrategyInterface
+{
+    public function getDeliveryCharge(float $subtotal): float
+    {
+        if ($subtotal >= 90.0) {
+            return 0.0;
+        }
+        if ($subtotal >= 50.0) {
+            return 2.95;
+        }
+        return 4.95;
+    }
+}
